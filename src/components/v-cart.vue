bcd4737f5fccdff67b110c18a8b0819fe960abb6
runardvl <runarshakirov@gmail.com>
2020-10-18 22:31:37 +0300

    dev добавил возможность подстчета 'Total' в корзине. Добавил кнопки '+' и '-' на товарах в корзине

--- src/components/v-cart.vue
+++ src/components/v-cart.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="v-cart">
     <router-link :to="{ name: 'catalog' }">
-      <div class="v-catalog__link_to_cart">Back to Catalog</div>
+      <div class="v-cart__link_to_cart">Back to Catalog</div>
     </router-link>
     <h1>Cart</h1>
     <p v-if="!cart_data.length">There are no products in cart...</p>
@@ -10,7 +10,13 @@
       :key="item.id"
       :cart_item_data="item"
       @deleteFromCart="deleteFromCart(index)"
+      @increment="increment(index)"
+      @decrement="decrement(index)"
     />
+    <div class="v-cart__total">
+      <p class="total__name">Total:</p>
+      <p>{{ cartTotalCost }} ₽</p>
+    </div>
   </div>
 </template>
 
@@ -31,9 +37,35 @@ export default {
   data() {
     return {};
   },
-  computed: {},
+  computed: {
+    cartTotalCost() {
+      let result = [];
+
+      if (this.cart_data.length) {
+        for (let item of this.cart_data) {
+          result.push(item.price * item.quantity);
+        }
+        result = result.reduce(function(sum, el) {
+          return sum + el;
+        });
+        return result;
+      } else {
+        return 0;
+      }
+    }
+  },
   methods: {
-    ...mapActions(["DELETE_FROM_CART"]),
+    ...mapActions([
+      "DELETE_FROM_CART",
+      "INCREMENT_CART_ITEM",
+      "DECREMENT_CART_ITEM"
+    ]),
+    increment(index) {
+      this.INCREMENT_CART_ITEM(index);
+    },
+    decrement(index) {
+      this.DECREMENT_CART_ITEM(index);
+    },
     deleteFromCart(index) {
       this.DELETE_FROM_CART(index);
     }
@@ -42,11 +74,29 @@ export default {
 </script>
 
 <style lang="scss">
-.v-catalog__link_to_cart {
-  position: absolute;
-  top: 10px;
-  right: 10px;
-  padding: $padding * 2;
-  border: solid 1px #aeaeae;
+.v-cart {
+  margin-bottom: 121px;
+  &__link_to_cart {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    padding: $padding * 2 $padding * 3;
+    border: solid 1px $green-bg;
+  }
+  &__total {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    left: 0;
+    padding: $padding * 3;
+    display: flex;
+    justify-content: center;
+    background-color: $green-bg;
+    color: #fff;
+    font-size: 20px;
+  }
+  .total__name {
+    margin-right: $margin * 2;
+  }
 }
 </style>
